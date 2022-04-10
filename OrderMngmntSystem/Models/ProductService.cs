5bb4dcadf01bfa0b074d8ccfe4e34d3d63a4ce48
Harshit784 <70321537+Harshit784@users.noreply.github.com>
2022-04-10 14:35:22 +0530

    Add project files.

---
+++ OrderMngmntSystem/Models/ProductService.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderMngmntSystem
+{
+    public class ProductService
+    {
+        public string productName { get; set; }
+        [Key]
+        public int productId { get; set; }
+
+        public string productCategory { get; set; }
+    }
+}
